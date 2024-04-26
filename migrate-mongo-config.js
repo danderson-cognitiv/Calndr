609c0e9cd7543bfd008431d9e18c4f45ee8dc0d4
Dave Anderson <>
2024-04-26 09:58:57 -0700

    wip

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -6,7 +6,6 @@ const dbPassword = process.env.DB_PASSWORD;
 const mongoDBConnection = 'mongodb://' + dbUser + ':' + encodeURIComponent("test") + process.env.DB_INFO;
 console.log(mongoDBConnection);
 
-// migrate-mongo-config.js
 module.exports = {
     mongodb: {
       url: mongoDBConnection,
@@ -14,17 +13,14 @@ module.exports = {
       databaseName: "cal",
     },
   
-    // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+    // The migrations dir, can be an relative or absolute path
     migrationsDir: "database/migrations",
   
-    // The MongoDB collection where the applied changes are stored. Only edit this when really necessary.
     changelogCollectionName: "changelog",
   
-    // The file extension to create migrations and search for in migration dir
+    // The file extension
     migrationFileExtension: ".js",
   
-    // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
-    // if the file should be run.  Requires that scripts are coded to be idempotent.
     useFileHash: false
   };
   
