79543a7cc1ff5e15cc7bc6ae5ea3f0b23b0f30e7
Emily Lee <emilysoolee@gmail.com>
2024-05-13 13:48:02 -0700

    added more user events data for demo

--- database/migrations/V02_createEvents.js
+++ database/migrations/V02_createEvents.js
@@ -9,8 +9,6 @@ module.exports = {
       const fladenbrot420Id = await db.collection('users').findOne({ username: "Fladenbrot420" });
       const emilitId = await db.collection('users').findOne({ username: "Emilit" });
       const angeloalfonsoId = await db.collection('users').findOne({ username: "AngeloAlfonso" });
-      const wesleywongId = await db.collection('users').findOne({ username: "Wes1004" });
-      const bensonwakabaId = await db.collection('users').findOne({ username: "Benson143" });
   
       await db.collection('events').insertMany([
         {
@@ -20,6 +18,22 @@ module.exports = {
           endTime: new Date("2024-11-17T00:00:00"),
           location: "420 69th Ave, Seattle WA, 98119",
           description: "Hey guys, I'm turning 30 pretty soon come hang out with me as I celebrate!"
+        },
+        {
+          createdBy: dandyAndy77Id._id,
+          name: "Dune Movie Night",
+          startTime: new Date("2024-05-07T19:00:00"),
+          endTime: new Date("2024-05-07T21:00:00"),
+          location: "Pacific Science Center",
+          description: "Dune 2 finally came out. Lets go watch it!"
+        },
+        {
+          createdBy: dandyAndy77Id._id,
+          name: "Calndr Presentation",
+          startTime: new Date("2024-05-16T18:00:00"),
+          endTime: new Date("2024-05-16T20:00:00"),
+          location: "Bannan 429",
+          description: "We got this!"
         },
         {
           createdBy: fladenbrot420Id._id,
@@ -40,58 +54,50 @@ module.exports = {
         {
           createdBy: emilitId._id,
           name: "Emily's Housewarming Party!",
-          startTime: new Date("2024-09-09T19:00:00"),
-          endTime: new Date("2024-09-10T00:00:00"),
+          startTime: new Date("2024-06-09T15:00:00"),
+          endTime: new Date("2024-06-19T21:00:00"),
           location: "159th 21st St, Seattle, WA 98122",
           description: "Ya girl finally got her own place. Come check out my roof and celebrate with me!"
         },
         {
           createdBy: emilitId._id,
           name: "Emily's Birthday Party!",
-          startTime: new Date("2024-11-29T19:00:00"),
-          endTime: new Date("2024-11-30T00:00:00"),
+          startTime: new Date("2024-05-13T19:00:00"),
+          endTime: new Date("2024-05-14T00:00:00"),
           location: "159th 21st St, Seattle, WA 98122",
           description: "It's Emilit! Come celebrate my birthday with me at my new place!"
         },
         {
           createdBy: emilitId._id,
           name: "I got the internship!",
-          startTime: new Date("2024-11-29T19:00:00"),
-          endTime: new Date("2024-11-30T00:00:00"),
+          startTime: new Date("2024-05-07T14:00:00"),
+          endTime: new Date("2024-05-07T18:00:00"),
           location: "Meet Korean BBQ",
           description: "Celebrating a job lined up finally!"
         },
         {
-          createdBy: wesleywongId._id,
-          name: "Pre-graduation Party!",
-          startTime: new Date("2024-06-04T19:00:00"),
-          endTime: new Date("2024-06-05T00:00:00"),
-          location: "Chieftan's",
-          description: "Grabbing drinks with the classmates before the ceremony"
+          createdBy: emilitId._id,
+          name: "Group Therapy Weekender at the Gorge!",
+          startTime: new Date("2024-05-24T19:00:00"),
+          endTime: new Date("2024-05-26T19:00:00"),
+          location: "The Gorge Amphitheater",
+          description: "Who's ready for another weekend of Above and Beyond?!"
         },
         {
-          createdBy: wesleywongId._id,
+          createdBy: angeloalfonsoId._id,
+          name: "LeetCode Sessions",
+          startTime: new Date("2024-05-28T12:00:00"),
+          endTime: new Date("2024-05-28T18:00:00"),
+          location: "SU Campus, LEML 240",
+          description: "Lets prep for coding interviews!"
+        },
+        {
+          createdBy: angeloalfonsoId._id,
           name: "Mox",
-          startTime: new Date("2024-07-04T19:00:00"),
-          endTime: new Date("2024-07-05T00:00:00"),
+          startTime: new Date("2024-05-04T19:00:00"),
+          endTime: new Date("2024-005-05T00:00:00"),
           location: "Mox Boarding House",
           description: "Reunion with the classmates over table top games!"
-        },
-        {
-          createdBy: bensonwakabaId._id,
-          name: "Benson's Birthday Party!",
-          startTime: new Date("2024-08-07T19:00:00"),
-          endTime: new Date("2024-07-08T00:00:00"),
-          location: "Umi Sake House",
-          description: "Celebrate my 23rd Birthday with me at Umi! I reserved the private room!"
-        },
-        {
-          createdBy: bensonwakabaId._id,
-          name: "I heard back from Amazon! Lets Celebrate",
-          startTime: new Date("2024-05-02T19:00:00"),
-          endTime: new Date("2024-05-03T00:00:00"),
-          location: "Hillside Bar",
-          description: "I finally heard back from Amazon. Lets celebrate!!"
         },
       ]);
     },
--- database/migrations/V03_createUserEvents.js
+++ database/migrations/V03_createUserEvents.js
@@ -1,26 +1,24 @@
 module.exports = {
     async up(db, client) {
       await db.createCollection('userEvents');
-  
       await db.collection('userEvents').deleteMany({});
   
       const daveId = await db.collection('users').findOne({ username: "DandyAndy77" });
       const mahirId = await db.collection('users').findOne({ username: "Fladenbrot420" });
       const emilitId = await db.collection('users').findOne({ username: "Emilit" });
       const angeloId = await db.collection('users').findOne({ username: "AngeloAlfonso" });
-      const wesId = await db.collection('users').findOne({ username: "Wes1004" });
-      const benId = await db.collection('users').findOne({ username: "Benson143" });
 
       const davesBirthdayEventId = await db.collection('events').findOne({ name: "Dave's Birthday Party!" });
+      const davesDuneMovieEventId = await db.collection('events').findOne({ name: "Dune Movie Night" });
+      const davesPresEventId = await db.collection('events').findOne({ name: "Calndr Presentation" });
       const mahirGraduationEventId = await db.collection('events').findOne({ name: "Mahir's Graduation party" });
       const mahirSecretPartyEventId = await db.collection('events').findOne({ name: "Mahir's secret after party" });
       const emilysHousewarmigEventId = await db.collection('events').findOne({ name: "Emily's Housewarming Party!" });
       const emilysBirthdayEventId = await db.collection('events').findOne({ name: "Emily's Birthday Party!" });
       const emilysInternshipCelebrationEventId = await db.collection('events').findOne({ name: "I got the internship!" });
-      const wesleysPregradEventId = await db.collection('events').findOne({ name: "Pre-graduation Party!" });
-      const wesleysMoxEventId = await db.collection('events').findOne({ name: "Mox" });
-      const bensonsBirthdayEventId = await db.collection('events').findOne({ name: "Benson's Birthday Party!" });
-      const bensonsNewCareerEventId = await db.collection('events').findOne({ name: "I heard back from Amazon! Lets Celebrate" });
+      const emilysGroupTherapyEventtId = await db.collection('events').findOne({ name: "Group Therapy Weekender at the Gorge!" });
+      const angelosMoxEventId = await db.collection('events').findOne({ name: "Mox" });
+      const angelosLeetCodeId = await db.collection('events').findOne({ name: "LeetCode Sessions" });
       
       await db.collection('userEvents').insertMany([
         {
@@ -44,9 +42,54 @@ module.exports = {
           rsvp: true
         },
         {
+          event: davesDuneMovieEventId._id,
+          user: daveId._id,
+          rsvp: true
+        },
+        {
+          event: davesDuneMovieEventId._id,
+          user: mahirId._id,
+          rsvp: true
+        },
+        {
+          event: davesDuneMovieEventId._id,
+          user: emilitId._id,
+          rsvp: true
+        },
+        {
+          event: davesDuneMovieEventId._id,
+          user: angeloId._id,
+          rsvp: true
+        },
+        {
+          event: davesPresEventId._id,
+          user: daveId._id,
+          rsvp: true
+        },
+        {
+          event: davesPresEventId._id,
+          user: mahirId._id,
+          rsvp: true
+        },
+        {
+          event: davesPresEventId._id,
+          user: emilitId._id,
+          rsvp: true
+        },
+        {
+          event: davesPresEventId._id,
+          user: angeloId._id,
+          rsvp: true
+        },
+        {
+          event: mahirGraduationEventId._id,
+          user: mahirId._id,
+          rsvp: true
+        },
+        {
           event: mahirGraduationEventId._id,
           user: daveId._id,
-          rsvp: false
+          rsvp: true
         },
         {
           event: mahirGraduationEventId._id,
@@ -60,7 +103,12 @@ module.exports = {
         },
         {
           event: mahirSecretPartyEventId._id,
-          user: mahirId._id,
+          user: daveId._id,
+          rsvp: true
+        },
+        {
+          event: mahirSecretPartyEventId._id,
+          user: angeloId._id,
           rsvp: true
         },
         {
@@ -70,7 +118,17 @@ module.exports = {
         },
         {
           event: emilysHousewarmigEventId._id,
+          user: emilitId._id,
+          rsvp: true
+        }, 
+        {
+          event: emilysHousewarmigEventId._id,
           user: mahirId._id,
+          rsvp: true
+        }, 
+        {
+          event: emilysBirthdayEventId._id,
+          user: emilitId._id,
           rsvp: true
         }, 
         {
@@ -80,7 +138,7 @@ module.exports = {
         }, 
         {
           event: emilysBirthdayEventId._id,
-          user: wesId._id,
+          user: mahirId._id,
           rsvp: true
         }, 
         {
@@ -91,51 +149,76 @@ module.exports = {
         {
           event: emilysInternshipCelebrationEventId._id,
           user: angeloId._id,
-          rsvp: false
+          rsvp: true
         }, 
         {
-          event: wesleysPregradEventId._id,
-          user: benId._id,
-          rsvp: false
+          event: emilysInternshipCelebrationEventId._id,
+          user: daveId._id,
+          rsvp: true
         }, 
         {
-          event: wesleysPregradEventId._id,
+          event: emilysInternshipCelebrationEventId._id,
+          user: mahirId._id,
+          rsvp: true
+        }, 
+        {
+          event: angelosMoxEventId._id,
           user: angeloId._id,
           rsvp: true
         }, 
         {
-          event: wesleysMoxEventId._id,
+          event: angelosMoxEventId._id,
           user: daveId._id,
-          rsvp: false
+          rsvp: true
         }, 
         {
-          event: wesleysMoxEventId._id,
+          event: angelosMoxEventId._id,
           user: emilitId._id,
           rsvp: true
         }, 
         {
-          event: wesleysMoxEventId._id,
-          user: benId._id,
+          event: angelosMoxEventId._id,
+          user: mahirId._id,
           rsvp: true
         }, 
         {
-          event: bensonsBirthdayEventId._id,
+          event: angelosLeetCodeId._id,
+          user: angeloId._id,
+          rsvp: true
+        }, 
+        {
+          event: angelosLeetCodeId._id,
+          user: daveId._id,
+          rsvp: true
+        }, 
+        {
+          event: angelosLeetCodeId._id,
+          user: mahirId._id,
+          rsvp: true
+        }, 
+        {
+          event: angelosLeetCodeId._id,
           user: emilitId._id,
           rsvp: true
         }, 
         {
-          event: bensonsBirthdayEventId._id,
-          user: mahirId._id,
-          rsvp: false
-        }, 
-        {
-          event: bensonsNewCareerEventId._id,
-          user: wesId._id,
+          event: emilysGroupTherapyEventtId._id,
+          user: emilitId._id,
           rsvp: true
         }, 
         {
-          event: bensonsNewCareerEventId._id,
+          event: emilysGroupTherapyEventtId._id,
+          user: daveId._id,
+          rsvp: true
+        }, 
+        {
+          event: emilysGroupTherapyEventtId._id,
           user: mahirId._id,
+          rsvp: true
+        }, 
+        {
+          event: emilysGroupTherapyEventtId._id,
+          user: angeloId._id,
           rsvp: true
         }, 
       ]);
