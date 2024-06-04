3bb858ec976a4a5c04371d7e2002fde95fb34d0d
Dave Anderson <>
2024-06-03 20:34:47 -0700

    authservice

--- client/src/app/AuthService.ts
+++ client/src/app/AuthService.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { CalndrProxyService } from './proxies/calndrproxy.service';
 import { IUserModel } from '../../../database/interfaces/IUserModel';
-import { Router } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -19,14 +19,22 @@ export class AuthService {
     console.log('Loading current user');
     this.calndrProxyService.getCurrentUser().subscribe({
       next: (user) => {
-        this.currentUserSubject.next(user);
+        if (user) {
+          this.currentUserSubject.next(user);
+        } else {
+          this.handleUserNotFound();
+        }
       },
       error: (error) => {
         console.error('Failed to load current user:', error);
-        this.currentUserSubject.next(null);
-        this.router.navigate(['/login']);
+        this.handleUserNotFound();
       }
     });
+  }
+
+  private handleUserNotFound(): void {
+    this.currentUserSubject.next(null);
+    this.router.navigate(['/login']);
   }
 
   login(): void {
@@ -44,5 +52,15 @@ export class AuthService {
         console.error('Failed to logout:' + error);
       }
     });
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const user = this.currentUserSubject.value;
+    if (user) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
 }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { SettingsComponent } from './settings/settings.component';
 import { EventChatComponent } from './event-chat/event-chat.component';
 import { MyEventsComponent } from './my-events/my-events.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './AuthService';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'event/:userEventId', component: EventChatComponent },
-  { path: 'events', component: MyEventsComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthService] },
+  { path: 'event/:userEventId', component: EventChatComponent, canActivate: [AuthService] },
+  { path: 'events', component: MyEventsComponent, canActivate: [AuthService]},
   { path: 'settings', component: SettingsComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' } // Catch-all route to redirect to home
